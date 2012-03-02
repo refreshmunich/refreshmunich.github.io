fb94179b9e99f2a54891cb2df0876dbad925d717
Elias Klughammer <elias.klughammer@me.com>
2012-03-02 13:34:26 +0100

    Next Event comes from Google Calendar

--- js/app.js
+++ js/app.js
@@ -25,11 +25,63 @@ $(document).ready(function () {
     members.innerHTML = build_list(data.users);
   };
 
-
   var $faq = $('#faq');
   $faq.find('h3').click(function(e) {
     $(e.target).siblings('.answer').slideToggle();
   });
+
+  // --- Load the next event from google calendar
+  $.getJSON('http://www.google.com/calendar/feeds/refreshmunich@googlemail.com/public/full?callback=?', {
+    'alt':'json-in-script',
+    'orderby':'starttime',
+    'max-results':'1',
+    'singleevents':'true',
+    'sortorder':'ascending',
+    'futureevents':'true'
+  }, function (data) {
+    var nextEvent = data.feed.entry[0];
+    
+    nextEvent && renderNextEvent(nextEvent);
+  });
+
+  function renderNextEvent(nextEvent) {
+    // --- Javascript doesnt know week days and month names so we got to teach it.
+    var weekdays         = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months           = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    var eventTitle       = nextEvent.title['$t'];
+    var eventDate        = new Date(nextEvent['gd$when'][0].startTime);
+    var eventWeekDay     = weekdays[eventDate.getDay()];
+    var eventDay         = eventDate.getDate();
+    var eventMonth       = months[eventDate.getMonth()];
+    var eventYear        = eventDate.getFullYear();
+    var eventHours       = eventDate.getHours();
+    var eventMinutes     = eventDate.getMinutes();
+
+    // --- We dont want a single zero
+    if (eventMinutes === 0)
+      eventMinutes = "00";
+
+    // --- Build the time string
+    var eventTime        = eventWeekDay + ', ' + eventDay + '. ' + eventMonth + ' ' + eventYear + ', ' + eventHours + ':' + eventMinutes;
+    
+    var eventLocation    = nextEvent['gd$where'][0].valueString;
+    var eventDescription = nextEvent.content['$t'];
+
+    // --- Make breaks in the description
+    eventDescription = eventDescription.replace(/\n/gm, "<br>");
+
+    // --- Apply the values in the UI
+    $('#eventTitle').html(eventTitle);
+    $('#eventTime').html(eventTime);
+    $('#eventLoaction').html(eventLocation);
+    $('#eventDescription').html(eventDescription);
+
+    // --- Finally show the next event box
+    $('#next-event').css({
+      height:"auto"
+    });
+  }
 
   var members = [];
 
