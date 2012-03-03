6c820d676279873dedb5421beca1668c8725b1ad
Elias Klughammer <elias.klughammer@me.com>
2012-03-03 20:07:09 +0100

    Re-enabled spinning logo

--- js/app.js
+++ js/app.js
@@ -253,7 +253,7 @@ $(document).ready(function () {
   $(window).scroll(function(event) {
     var scrollValue = $(this).scrollTop();
     var top         = 200 - scrollValue;
-    //var rotate      = scrollValue < 400 ? Math.floor(scrollValue / 4) : 0;
+    var rotate      = scrollValue < 400 ? scrollValue / 4 : 0;
 
     // --- If the logo is in perfect position, stop it
     top = Math.max(top, 0);
@@ -262,12 +262,14 @@ $(document).ready(function () {
       top:top
     });
 
-    // $('#bigLogo').css({
-    //   "-webkit-transform":"rotate(" + rotate + "deg)",
-    //   "-moz-transform":"rotate(" + rotate + "deg)",
-    //   "-ms-transform":"rotate(" + rotate + "deg)",
-    //   "-o-transform":"rotate(" + rotate + "deg)"
-    // });
+    var translateValue = "rotate(" + rotate + "deg) translateZ(0)";
+
+    $('#bigLogo').css({
+      "-webkit-transform":translateValue,
+      "-moz-transform":   translateValue,
+      "-ms-transform":    translateValue,
+      "-o-transform":     translateValue
+    });
   });
 
   // --- Insert current year in copyright
