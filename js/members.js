9c02e8ba7e56e082647674fa2f5e017d9031fa7c
Julie Ng <uxjulie@gmail.com>
2012-07-25 17:47:18 +0200

    Re-indenting and renaming function

--- js/members.js
+++ js/members.js
@@ -3,35 +3,37 @@ var MembersList = {
 
   init:function() {
     console.log("initialized");
-// Cache references to our template and DOM elements
-var membersTemplate = $.trim( $('#js-members-template').html() ),
-    membersLoading  = $('#js-members-loading'),
-    membersList     = $('#js-members');
+
+    // Cache references to our template and DOM elements
+    var membersTemplate = $.trim( $('#js-members-template').html() ),
+        membersLoading  = $('#js-members-loading'),
+        membersList     = $('#js-members');
+
+    
   },
 
- buildMembersList: function(users) {
+  buildHTML: function(users) {
+    // console.log(users);
+    var html = '',
+      length = users.length; // + 1, // plus one because we will add CTA to it below
+      join = {
+        'name': 'Want to join us?',
+        'screen_name': 'refreshmunich',
+        'description': 'Get in touch via <a href="http://www.twitter.com/refreshmunich" target="_blank">@twitter</a> and drop in on one of our next Stammtisches for beers and nerdy chatter.',
+        'profile_image_url': 'http://twimg0-a.akamaihd.net/profile_images/1866985909/icon200x200_normal.png'
+      };
 
-  console.log(users);
-  var html = '',
-    length = users.length; // + 1, // plus one because we will add CTA to it below
-    join = {
-      'name': 'Want to join us?',
-      'screen_name': 'refreshmunich',
-      'description': 'Get in touch via <a href="http://www.twitter.com/refreshmunich" target="_blank">@twitter</a> and drop in on one of our next Stammtisches for beers and nerdy chatter.',
-      'profile_image_url': 'http://twimg0-a.akamaihd.net/profile_images/1866985909/icon200x200_normal.png'
-    };
+    users.push(join);
 
-  users.push(join);
+    for (var i=0; i<length; i++) {
+      html += membersTemplate.replace( /{{handle}}/ig, users[i].screen_name )
+                   .replace( /{{name}}/ig, users[i].name )
+                   .replace( /{{desc}}/ig, users[i].description )
+                   .replace( /{{pic}}/ig, users[i].profile_image_url );
+    }
 
-  for (var i=0; i<length; i++) {
-    html += membersTemplate.replace( /{{handle}}/ig, users[i].screen_name )
-                 .replace( /{{name}}/ig, users[i].name )
-                 .replace( /{{desc}}/ig, users[i].description )
-                 .replace( /{{pic}}/ig, users[i].profile_image_url );
-  }
-
-  return html;
-},
+    return html;
+  },
 
 // --- Fire the Twitter API request
 // $.getJSON('https://api.twitter.com/1/lists/members.json?callback=?', {
