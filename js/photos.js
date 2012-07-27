46b9af97d9c03f727273f46853a22e8ff292dc31
Julie Ng <uxjulie@gmail.com>
2012-07-27 21:38:45 +0200

    Reorganiized photos JS

--- js/photos.js
+++ js/photos.js
@@ -1,50 +1,65 @@
 var Photos = {
 
   init: function() {
-    
+    // Flickr settings
+    this.setID   = '72157629231564646'; // our photostream
+    this.api_key = 'b9ea2bbfbae76e8986f7f480705b3d04';
+
+    this.photos         =  $('#js-photos');               // our photos container in the DOM
+    this.photosLoading  =  $('#js-photos-loading');
+    this.errorMsg       =  $('#js-photos-loading-error');
+    this.debug          =  true;                        // true enables console logging
+
+    this.loadPhotos();
   },
 
-  loadJSON: function() {
-
+  loadPhotos: function() {
+    $.getJSON('http://api.flickr.com/services/rest/?jsoncallback=?', {
+      method:'flickr.photosets.getPhotos',
+      api_key: this.api_key,
+      format:'json',
+      photoset_id:this.setID,
+    }).error(this.loadError)
+      .success(this.loadSuccess)
+      .complete(this.removeLoader);
   },
 
-  loadSuccess: function() {
+  loadSuccess: function(data) {
+    var self = Photos;
+    if(self.debug) {
+      console.log("Successfully loaded JSON from Flickr:");
+      console.log(data);
+    }
 
-  },
-
-  loadError: function() {
-
-  },
-
-  buildHTML: function() {
-
-  }
-
-};
-
-  // --- Load the photos from Flickr
-  var setId = '72157629231564646'
-
-  $.getJSON('http://api.flickr.com/services/rest/?jsoncallback=?', {
-    method:'flickr.photosets.getPhotos',
-    api_key:'b9ea2bbfbae76e8986f7f480705b3d04',
-    format:'json',
-    photoset_id:setId,
-  }, function (data) {
     var photos = data.photoset.photo;
-    photos.length && renderPhotos(photos);
-  });
+    photos.length && self.renderPhotos(photos); // if we have photos, render them.
 
-  function renderPhotos(photos) {
+  },
+
+  loadError: function(data) {
+    var self = Photos;
+    if(self.debug) {
+      console.log("Error loading JSON from Flickr:");
+      console.log(data);
+    }
+
+    self.errorMsg.show();
+  },
+
+  removeLoader: function() {
+    var self = Photos;
+    self.photosLoading.remove();
+  },
+
+  renderPhotos: function(photos) {
+
+    // loop through our photos in reverse () order
     for (var i = photos.length - 1; i >= 0; i--) {
       var photo = photos[i];
-
       var image = $('<img>');
-
       var baseUrl = 'http://farm' + photo.farm + 
                      '.staticflickr.com/' + photo.server + 
                      '/' + photo.id + '_' + photo.secret;
-
       var thumbUrl = baseUrl + '_t.jpg';
       var bigUrl   = baseUrl + '.jpg';
 
@@ -56,22 +71,13 @@ var Photos = {
       link.attr({
         'href':   bigUrl,
         'target': '_blank',
-        'class':  'fancybox',
-        'rel':    'gallery',
         'title':  photo.title
       });
       link.append(image);
 
-      $('#photoContainer').append(link);
-    };
-
-    // --- Remove loading indicator
-    $('#photos .loading').remove();
-
-    // --- Activate lightbox (fancybox)
-    // $('.fancybox').fancybox({
-    //   prevEffect:'none',
-    //   nextEffect:'none'
-    // });
+      this.photos.append(link);
+    }
   }
+
+};
 
