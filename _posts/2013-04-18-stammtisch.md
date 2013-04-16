7c38b44d2ec679dd2478b3aab3d40f96cbbc63c7
Julie Ng <uxjulie@gmail.com>
2013-04-16 22:57:16 +0200

    Use jekyll posts for events instead of previous JS/markdown combination

---
+++ _posts/2013-04-18-stammtisch.md
@@ -0,0 +1,12 @@
+---
+title: Stammtisch
+time: Thursday 18. April 2013, 19:00
+calendar_month: Apr
+calendar_date: 18
+description: Our April Stammtisch. From now on, we'll be meeting on the 2nd of last Thursday of every month.
+venue: |
+  Piu  
+  Preysingstrasse 20  
+  81667 München  
+  [www.piu-muenchen.de](http://www.piu-muenchen.de)
+---
--- js/app.js
+++ js/app.js
@@ -6,94 +6,10 @@ $(document).ready(function () {
   var agent = navigator.userAgent;
   isIE = !!check.test(agent);
 
-  loadEvents();
   observeScrolling();
+  MembersList.init();
+  Photos.init();
 });
-
-function loadEvents() {
-  $.ajax({
-    url:'/content/events.md',
-    success:renderNextEvent
-  });
-}
-
-function renderNextEvent(content) {
-  var events    = content.split(/\n---\n/gi);
-  var nextEvent = events[0];
-  var lines     = nextEvent.split(/\n/gi);
-  var dateText  = lines[0];
-  var date      = Date.parse(dateText);
-  var dateNow   = +new Date();
-  var tolerance = 6 * 60 * 60 * 1000;
-  var convert   = new Markdown.Converter().makeHtml;
-
-  var location = lines[4];
-  var address  = lines[5] + '\n' + lines[6];
-  var adressHtml = convert(address);
-  var description = nextEvent.split(lines[6])[1];
-  var descriptionHtml = convert(description);
-
-  $('#js-venue').html(location);
-  $('#js-address').html(adressHtml);
-  $('#js-description').html(descriptionHtml);
-
-  // --- If the current time is the event's start time plus 6 hours, it's 'old'
-  if (dateNow > (+date + tolerance)) {
-    // --- Do not render the next event
-    return renderPastEvents(events);
-  }
-  else {
-    // --- Remove the first event
-    events.splice(0, 1);
-    renderPastEvents(events);
-  }
-
-  // --- Month names because JS doesn't know them
-  var month = getMonthAbbr(date.getMonth());
-
-  $('#js-day').html(date.getDate());
-  $('#js-month').html(month);
-  $('#js-time-start').html(dateText);
-  $('#recent-event').removeClass('is-hidden');
-}
-
-function renderPastEvents(pastEvents) {
-  // --- Do not render past events because it's not finished (TODO)
-  return;
-
-  // var html = '';
-
-  // for (var i = 0; i < pastEvents.length; i++) {
-  //   var pastEvent = pastEvents[i];
-  //   var lines     = pastEvent.split(/\n/gi);
-
-  //   if (!lines[0])
-  //     lines.splice(0, 1);
-
-  //   var time     = lines[0];
-  //   var date     = Date.parse(time);
-  //   var type     = lines[2];
-  //   var location = lines[4];
-
-  //   html += '<li class="past">' +
-  //             '<time class="past-when" datetime="' + date + '">' +
-  //               '<span class="past-month">' + getMonthAbbr(date.getMonth()) + '</span>' +
-  //               '<span class="past-date">' + date.getDate() + '</span>' +
-  //             '</time>' +
-  //             '<p class="past-type">' + type + '</p>' +
-  //             '<p class="past-location">' + location + '</p>' +
-  //           '</li>';
-  // }
-
-  // $('.past-list').html(html);
-
-  // $('.past-events').removeClass('is-hidden');
-}
-
-function getMonthAbbr(integer) {
-  var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-  return months[integer];
-}
 
 // --- If user scrolls the page, do the following things
 function observeScrolling() {
@@ -112,5 +28,3 @@ function observeScrolling() {
   });
 }
 
-MembersList.init();
-Photos.init();
